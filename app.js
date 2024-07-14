2c0de2f313ea1664de9b97a627653626f73fac21
IrynaKr1 <krutsiryna@gmail.com>
2024-07-14 14:02:05 +0200

    add result animation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -15,5 +16,19 @@ form.addEventListener('submit', e => {
         }
     });
 
-    console.log(score);
+    // show results on page
+    scrollTo(0,0);
+    
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 25);
 });
+
